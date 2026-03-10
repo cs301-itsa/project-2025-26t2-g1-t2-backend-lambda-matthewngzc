006d7c07f3e45f65e7c810d538c972774ecd0ab3
EC2 Default User <ec2-user@ip-172-31-24-211.ec2.internal>
2026-03-10 06:39:12 +0000

    add headers

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "http://project-2025-26t2-g1-t2-s3.s3-website-us-east-1.amazonaws.com",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "GET,OPTIONS"
+        },
         body: JSON.stringify('Hello, this is your AWS Lambda API response for your CS301 ITSA class!'),
     };
     return response;
